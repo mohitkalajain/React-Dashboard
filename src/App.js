1a1a952c272e9d20a1a8c799b575afd370324b79
Mohit Kala <mohit.kala@sciffer.com>
2022-05-20 17:16:26 +0530

    First commint

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Admin Dashboard With Material Ui</p>
+      <p>Admin Dashboard With Material Ui Design</p>
     </div>
   );
 }
